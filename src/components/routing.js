fd722110774140365511b91c983f2262c74126c8
Tamana543 <tamanafarzami33@gmail.com>
2025-05-14 16:41:38 +0430

    routing page completed

--- src/components/routing.js
+++ src/components/routing.js
@@ -5,6 +5,14 @@ import Reservation from "./pages/BookingPage";
 import Order from "./pages/Order";
 import Login from "./pages/Login";
 import Confirmation from "./pages/Confirmation";
+
 export default function Routing(){
-     
+ <Routes>
+     <Route path="/" element={<Homepage/>}/>
+     <Route path="/about" element={<About/>}/>
+     <Route path="/reservations" element={<Reservation/>}/>
+     <Route path="/order" element={<Order/>}/>
+     <Route path="/login" element={<Login/>}/>
+     <Route path="/confirmation" element={<Confirmation/>}/>
+ </Routes>    
 }
