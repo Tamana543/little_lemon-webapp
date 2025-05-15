e83b6ba9e75f4a25aac276b14ff2a78bf19ce7d0
Tamana543 <tamanafarzami33@gmail.com>
2025-05-15 14:21:29 +0430

    BookingPage Completed

--- src/components/pages/BookingPage.js
+++ src/components/pages/BookingPage.js
@@ -3,5 +3,17 @@ import ReservationForm from "../sections/reservePages/BookingForm"
 import { useReducer } from "react";
 import {fetchAPI} from "../../bookingsAPI"
 export default function BookingPage(){
-     
+     function updateTimes(date){
+          return fetchAPI(date)
+     }
+
+     const output = fetchAPI(new Date()) ;
+     const [availableTimes , dispatch] = useReducer(updateTimes,output) ;
+
+     return (
+          <>
+          <Heading/>
+          <ReservationForm availableTimes={availableTimes} updateTimes={dispatch}/>
+          </>
+     )
 }
