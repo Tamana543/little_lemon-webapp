8c73e1a3e4739428854f0fe0658ed4b1f2459911
Tamana543 <tamanafarzami33@gmail.com>
2025-05-17 18:01:30 +0430

    Booking Form Page Updated

--- src/components/sections/reservePages/BookingForm.js
+++ src/components/sections/reservePages/BookingForm.js
@@ -1,3 +1,17 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-export default function ReservationForm(props){}
+export default function ReservationForm(props){
+     const [fName, setFName] = useState("")
+     const [LName, setLName] = useState("")
+     const [email, setEmail] = useState("")
+     const [tel, setTel] = useState("")
+     const [people, setPeople] = useState(1)
+     const [date, setDate] = useState("")
+     const [occasion, setOccasion] = useState("")
+     const [preferences, setPreferences] = useState("")
+     const [comments, setComments] = useState("")
+
+     const [finalTime, setFinalTime] = useState(
+          props.availableTimes.map((times)=> <option>{times}</option>)
+     )
+}
