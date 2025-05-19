fa59e6715c6dff5dd9065378b926e6fb004ade1a
Tamana543 <tamanafarzami33@gmail.com>
2025-05-19 14:42:28 +0430

    booking page updated

--- src/components/pages/BookingPage.js
+++ src/components/pages/BookingPage.js
@@ -1,19 +1,20 @@
-import Heading  from "../sections/reservePages/Heading";
-import ReservationForm from "../sections/reservePages/BookingForm"
+import Heading from "../sections/reservePages/Heading";
+import ReservationForm from "../sections/reservePages/BookingForm";
 import { useReducer } from "react";
-import {fetchAPI} from "../../bookingsAPI"
-export default function BookingPage(){
-     function updateTimes(date){
-          return fetchAPI(date)
-     }
+import { fetchAPI } from "../../bookingsAPI";
+export default function BookingPage() {
+  function updateTimes(date) {
+    return fetchAPI(date);
+  }
 
-     const output = fetchAPI(new Date()) ;
-     const [availableTimes , dispatch] = useReducer(updateTimes,output) ;
+  const output = fetchAPI(new Date());
 
-     return (
-          <>
-          <Heading/>
-          <ReservationForm availableTimes={availableTimes} updateTimes={dispatch}/>
-          </>
-     )
+  const [availableTimes, dispatch] = useReducer(updateTimes, output);
+
+  return (
+    <>
+      <Heading />
+      <ReservationForm availableTimes={availableTimes} updateTimes={dispatch} />
+    </>
+  );
 }
