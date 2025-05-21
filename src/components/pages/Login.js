701a2c28a0e911a9fd59edc417acfad99636e511
Tamana543 <tamanafarzami33@gmail.com>
2025-05-21 16:57:22 +0430

    Style Updated

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,6 +3,22 @@ export default function Order(){
  return(
      <>
        <Login/>
+         <div class="window log_in-window hide">
+      <div class="closeBtn">X</div>
+      <form method="post" class="flex">
+        <h3 class="head">Log In</h3>
+        <div class="name flex">
+          <label for="name">Name</label>
+          <input type="text" name="name" id="Name" />
+        </div>
+        <div class="last_name flex">
+          <label for="last-name" class="last_name-label">Last Name</label>
+          <input type="text" name="last-name" id="lastName" />
+        </div>
+
+        <button type="submit" class="submit">Submit</button>
+      </form>
+    </div>
      </>
  )    
 }
--- src/components/sections/loginPages/Login.js
+++ src/components/sections/loginPages/Login.js
@@ -12,5 +12,7 @@ export default function Login() {
           </div>
           
      </header>
+     
+     
      );
 }
