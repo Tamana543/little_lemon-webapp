7ba9544716b4bbc281d58e7a2385c00024e398e9
Tamana543 <tamanafarzami33@gmail.com>
2025-05-21 17:03:54 +0430

    style updated

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,22 +3,20 @@ export default function Order(){
  return(
      <>
        <Login/>
-         <div class="window log_in-window hide">
-      <div class="closeBtn">X</div>
-      <form method="post" class="flex">
-        <h3 class="head">Log In</h3>
+        
+      <form method="post" class="flex form_login">
         <div class="name flex">
-          <label for="name">Name</label>
+          <label for="name" className=" label_login">Name</label>
           <input type="text" name="name" id="Name" />
         </div>
         <div class="last_name flex">
-          <label for="last-name" class="last_name-label">Last Name</label>
+          <label for="last-name" className="last_name-label label_login">Last Name</label>
           <input type="text" name="last-name" id="lastName" />
         </div>
 
         <button type="submit" class="submit">Submit</button>
       </form>
-    </div>
+   
      </>
  )    
 }
