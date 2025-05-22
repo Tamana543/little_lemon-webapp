203c0f3ef9ad17d2df410adf53071bf8160a9d74
Tamana543 <tamanafarzami33@gmail.com>
2025-05-22 16:45:40 +0430

    About Page Style Completed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,28 @@
 import Heading from "../sections/aboutPages/Heading"
+
+function hiddenTextEn(){
+     
+     const text = document.querySelector("#more")
+     const moreBtn = document.querySelector("#moreBtn")
+     const lessBtn = document.querySelector("#lessBtn")
+     // const moreBtn = document.querySelector(".")
+     text.classList.toggle("hidden")
+     moreBtn.classList.toggle("hidden")
+     lessBtn.classList.toggle("hidden")
+     
+
+}
 export default function About(){
      return(
           <>
           <Heading/>
           <div className="about_aboutPage flex">
                <h3 className="about_aboutPage-heading">Little Lemon</h3>
-               <p className="about_aboutPage-paragraph">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later.<span className="hidden">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.
+               <p className="about_aboutPage-paragraph">Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago, and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later....<span className="hidden" id="more">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.
 To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.
 </span></p>
-               <button className="action-button spe">Learn More</button>
+               <button className="action-button spe" onClick={hiddenTextEn} id="moreBtn">Learn More</button>
+               <button className="action-button spe hidden" onClick={hiddenTextEn} id="lessBtn">Learn Less</button>
           </div>
 
           </>
